05fc91ddd6347e9492481328ba9d188075059b02
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-12-16 14:29:38 +0630

    send message to sky

---
+++ send_msg_to_skype.py
@@ -0,0 +1,26 @@
+from skpy import Skype, SkypeMsg
+
+mail_address = "bot.team1dott@gmail.com"
+passwd = "team1dott"
+
+# Dott-Team1
+channel_id = "19:f4830852759d4c5f963e11588d888dac@thread.skype"
+
+def post_group_msg():
+  
+    sk = Skype(mail_address, passwd)
+
+    channel = sk.chats.chat(channel_id)
+
+    # plain-text message
+    content = " 3:30 မှာ meeting လုပ်ကြမယ်နော်.."
+
+    # skype_obj = Skype('scm.wutyitheint@gmail.com', '1998scmwyt')
+    # skype_obj.chats
+    # skype_obj.chats.recent()
+    # print(skype_obj.chats.recent(), '----------')
+   
+    channel.sendMsg(SkypeMsg.mention_all + content, rich=True)
+
+if __name__ == '__main__':
+    post_group_msg()
